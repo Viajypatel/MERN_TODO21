87627cb6253d13a5e2cde9cb834b4f38ae05efa7
Vijay Patel <97840681+Viajypatel@users.noreply.github.com>
2025-02-15 14:08:29 +0530

    Update authApi.js

--- Frontend/src/api/authApi.js
+++ Frontend/src/api/authApi.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
+const BASE_URL = "https://mern-todo-21-api.vercel.app";
 
-export const registerUser=async(userData)=>{
-     
-    return axios.post(`/api/auth/register`,userData);
+export const registerUser = async (userData) => {
+  return axios.post(`${BASE_URL}/api/auth/register`, userData);
 }
 
-export const loginUser=async(userData)=>{     
-    return axios.post(`/api/auth/login`,userData);
+export const loginUser = async (userData) => {
+  return axios.post(`${BASE_URL}/api/auth/login`, userData);
 }
